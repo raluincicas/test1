d9535220b51449defddbc61ae93e2537eb485282
Raluca Incicas <ralucaincicas@Ralucas-MacBook-Pro.local>
2019-06-13 16:48:03 +0300

    image to base642

--- app.py
+++ app.py
@@ -179,9 +179,9 @@ def create_app(test_config=None):
                     filename = secure_filename(image.filename)
                     
         
-        #image.save(os.path.join(app.config["IMAGE_UPLOADS"], filename))
+                    image.save(os.path.join(app.config["IMAGE_UPLOADS"], filename))
                     img = Image.open(image.stream)
-                    with open(image.filename, "rb") as imageFile:
+                    with open(image, "rb") as imageFile:
                         str = base64.b64encode(imageFile.read())
                         print(str)
                     
